1e183bef8dfdbe2d68ac236cd5ae2306ea0d482f
Sarngond <allenrader2002@gmail.com>
2019-11-14 22:22:15 -0600

    navMesh waypoint system set up, needs to be tweeked

--- OrbGame/Assets/Guards/Guard.cs
+++ OrbGame/Assets/Guards/Guard.cs
@@ -6,27 +6,56 @@ using UnityEngine.AI;
 public class Guard : MonoBehaviour
 {
     public float speed = 5;
+    public float patrolSpeed = 3;
     public float waitTime = 0.3f;
     public float turnSpeed = 90;
     public bool onPatrol = true;
 
+    public float followStopDistance = 5;
+
     public Transform pathHolder;
     public GameObject player;
 
+    public Transform[] points;
+    private int destPoint = 0;
     private NavMeshAgent navAgent;
+
     private bool atTargetWaypoint = false;
 
     void Start() {
         navAgent = GetComponent<NavMeshAgent>();
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
 
-        SetWaypoints();
+        GotoNextPoint();
+
+        //SetWaypoints();
     }
 
     void Update() {
-        //FollowPlayer();
+        FollowPlayer();
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!navAgent.pathPending && navAgent.remainingDistance < 0.5f && onPatrol)
+            GotoNextPoint();
     }
 
-    IEnumerator FollowPath(Vector3[] waypoints) {
+    void GotoNextPoint() {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+        // Set the agent to go to the currently selected destination.
+        navAgent.destination = points[destPoint].position;
+        navAgent.speed = patrolSpeed;
+        navAgent.stoppingDistance = 0f;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    /*IEnumerator FollowPath(Vector3[] waypoints) {
         transform.position = waypoints[0];
 
         int targetWaypointIndex = 1;
@@ -36,12 +65,23 @@ public class Guard : MonoBehaviour
 
         while (true) {
             if (onPatrol) {
+                navAgent.enabled = false;
                 StartCoroutine(TurnToFace(targetWaypoint));
                 //navAgent.SetDestination(targetWaypoint);
                 transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
-                Debug.Log("ah");
+                //Debug.Log("ah");
+                //Debug.Log(atTargetWaypoint);
+
+                //if (atTargetWaypoint) {
+                //    navAgent.enabled = true;
+                //}
+                //if(!atTargetWaypoint) {
+                //    navAgent.enabled = false;
+                //}
+
                 if (transform.position == targetWaypoint) {
-                    Debug.Log("at target");
+                    //atTargetWaypoint = true;
+                    //Debug.Log(atTargetWaypoint);
                     targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
                     targetWaypoint = waypoints[targetWaypointIndex];
                     yield return new WaitForSeconds(waitTime);
@@ -51,6 +91,7 @@ public class Guard : MonoBehaviour
             } else if(!onPatrol) {
                 navAgent.enabled = true;
                 navAgent.SetDestination(player.transform.position);
+                //atTargetWaypoint = false;
             }
             yield return null;
         }
@@ -65,7 +106,7 @@ public class Guard : MonoBehaviour
             transform.eulerAngles = Vector3.up * angle;
             yield return null;
         }
-    }
+    }*/
 
     private void OnDrawGizmos() {
         Vector3 startPosition = pathHolder.GetChild(0).position;
@@ -79,7 +120,7 @@ public class Guard : MonoBehaviour
         Gizmos.DrawLine(previousPosition, startPosition);
     }
 
-    private void SetWaypoints() {
+    /*private void SetWaypoints() {
 
         Vector3[] waypoints = new Vector3[pathHolder.childCount];
         for (int i = 0; i < waypoints.Length; i++) {
@@ -88,14 +129,16 @@ public class Guard : MonoBehaviour
         }
 
         StartCoroutine(FollowPath(waypoints));
-    }
+    }*/
 
-    /*private void FollowPlayer() {
+    private void FollowPlayer() {
 
         if (!onPatrol) {
-            navAgent.enabled = true;
+            //navAgent.enabled = true;
             navAgent.SetDestination(player.transform.position);
+            navAgent.speed = speed;
+            navAgent.stoppingDistance = followStopDistance;
         }
-    }*/
+    }
 
 }
